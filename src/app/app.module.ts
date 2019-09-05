2a684f20e8e958f77b239b155231029c9e535b79
Adam Benjamin <benjamam@miamioh.edu>
2019-09-05 14:47:05 +0000

    basic set up + redux for watchlist feature module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,7 @@ import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { reducers } from './reducers';
 import { EffectsModule } from '@ngrx/effects';
 import { CounterEffects } from './effects/counter.effects';
+import { WatchlistModule } from './features/watchlist/watchlist.module';
 
 @NgModule({
   declarations: [
@@ -26,6 +27,7 @@ import { CounterEffects } from './effects/counter.effects';
     BrowserModule,
     AppRoutingModule,
     TodoModule,
+    WatchlistModule,
     StoreModule.forRoot(reducers),
     StoreDevtoolsModule.instrument(),
     EffectsModule.forRoot([CounterEffects])
--- src/app/effects/counter.effects.ts
+++ src/app/effects/counter.effects.ts
@@ -1,10 +1,26 @@
 import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { Injectable } from '@angular/core';
 import * as actions from '../actions/counter.actions';
-import { tap } from 'rxjs/operators';
+import * as appActions from '../actions/app.actions';
+import { tap, map, filter } from 'rxjs/operators';
 
 @Injectable()
 export class CounterEffects {
+
+  readCountByForReset$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(appActions.applicationStarted),
+      // read the value from localstarge
+      map(() => localStorage.getItem('by')),
+      // if it is null, do nothing
+      filter((val) => val !== null),
+      // if it isn't null
+      // convert it to a number (it's a string)
+      map(val => parseInt(val, 10)),
+      // dispatch a countBySet(x)
+      map(by => actions.setCountBy({ by }))
+    )
+  );
 
   writeCountByForReset$ = createEffect(() =>
     this.actions$.pipe(
